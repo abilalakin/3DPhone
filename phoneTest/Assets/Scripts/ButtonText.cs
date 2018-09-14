acbd474a99ebb950f26b3e3d7a45489f7718fd17
Bilal <bilal97@gmail.com>
2018-09-14 14:45:52 +0300

    3d phone first commit

---
+++ phoneTest/Assets/Scripts/ButtonText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonText : MonoBehaviour {
+
+	public Text display;
+	public bool isDisplay = false;
+	// Use this for initialization
+	void Start () {
+		display.text = "";
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DisplayText() {
+		
+		if (isDisplay) {
+			
+			display.text = "";
+			isDisplay = false;
+		}
+
+		else {
+			display.text = "Hoparlor";
+			isDisplay = true;
+
+		}		
+}
+
+	
+
+
+
+}
---
+++ phoneTest/Assets/Scripts/Rotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    float lockPos = 0;
+
+    float object_rotX,object_rotY;
+    void OnMouseDrag() {
+        
+        if (!Input.GetKey(KeyCode.LeftControl)) { // To move the object with left control and left mouse 
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(-pitch, -yaw, 0f);
+    
+   /* object_rotX = Input.GetAxis("Mouse X") * speedH * Time.deltaTime;
+    object_rotY = Input.GetAxis("Mouse Y") * speedV * Time.deltaTime;
+    transform.RotateAround(transform.position, new Vector3(object_rotY, -object_rotX, 0), 400 * Time.deltaTime);
+       */
+        }
+
+    
+    }
+
+    void Update() {
+
+     //   transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,lockPos);
+    }
+}
